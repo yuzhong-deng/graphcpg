e8733b17086544fb5cefb810497819ec8005b99e
YuZhong <57375920+EE-SIS@users.noreply.github.com>
2023-02-14 16:56:36 +0800

    Add files via upload

---
+++ data/Neuron-Mouse/downloadMouseLuo.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import re
+import tarfile
+import wget
+from tqdm import tqdm
+import os
+import time
+excel_df = pd.read_excel("Luo_mouse_download_titleANDlinks.xlsx", index_col=None, header=None)
+patternRandomPrimerIndex = r"(.*)AD008(.*)"
+link_list = []
+for id,title in enumerate(excel_df.iloc[0,:]):
+    if re.match(patternRandomPrimerIndex, title)!=None:
+        link_list.append(excel_df.iloc[1,id])
+output_directory = "E:/Mouse"
+for link in tqdm(link_list[:]):
+    download_link = 'https:' + link[4:]
+    download_flag = True
+    while download_flag:
+        try:
+            filename = wget.download(download_link, out=output_directory)
+            # print(filename)
+            download_flag = False
+        except:
+            print("Retry...")
+            time.sleep(1)
---
+++ data/Neuron-Mouse/untarMouseLuo.py
@@ -0,0 +1,14 @@
+from tqdm import tqdm
+import os
+import tarfile
+
+output_directory = "E:/Mouse"
+# Un-tar
+print("un_tar:")
+for file in tqdm(os.listdir(output_directory)):
+    # print(output_directory+'/'+file)
+    tar_filename = output_directory+'/'+file
+    tar = tarfile.open(tar_filename)
+    tar.extractall(output_directory)
+    tar.close()
+    os.remove(tar_filename)
