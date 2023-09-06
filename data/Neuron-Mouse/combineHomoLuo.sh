b570fedb838253bca92f3a61fb9570ab85580091
Bruce Deng <57375920+yuzhong-deng@users.noreply.github.com>
2023-09-06 16:47:31 +0800

    Update and rename combineHomoLuo.sh to combineMouseLuo.sh

--- data/Neuron-Mouse/combineHomoLuo.sh
+++
@@ -1,1 +0,0 @@
-python ../CombineEncodedLabels.py --y_files y_* --pos_files pos_* --y_outFile y_Luo_Homo.npz --pos_outFile pos_Luo_Homo.npz
---
+++ data/Neuron-Mouse/combineMouseLuo.sh
@@ -0,0 +1,1 @@
+python CombineEncodedLabels.py --y_files y_* --pos_files pos_* --y_outFile y_Luo_Mouse.npz --pos_outFile pos_Luo_Mouse.npz
