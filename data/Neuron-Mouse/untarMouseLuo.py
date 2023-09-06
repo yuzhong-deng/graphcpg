3a9cbf7d7e621dd99da93b3905665f1c11d883dd
Bruce Deng <57375920+yuzhong-deng@users.noreply.github.com>
2023-09-06 16:41:26 +0800

    Update untarMouseLuo.py

--- data/Neuron-Mouse/untarMouseLuo.py
+++ data/Neuron-Mouse/untarMouseLuo.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 import os
 import tarfile
 
-output_directory = "Mouse_folder"
+output_directory = os.getcwdb()
 # Un-tar
 print("un_tar:")
 for file in tqdm(os.listdir(output_directory)):
