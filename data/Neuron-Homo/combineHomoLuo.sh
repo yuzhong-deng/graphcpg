8f15c0215a2fa0190e914ebb0c4d7651ea7e0f6e
Bruce Deng <57375920+yuzhong-deng@users.noreply.github.com>
2023-09-06 16:38:59 +0800

    Update combineHomoLuo.sh

--- data/Neuron-Homo/combineHomoLuo.sh
+++ data/Neuron-Homo/combineHomoLuo.sh
@@ -1,1 +1,1 @@
-python ../CombineEncodedLabels.py --y_files y_* --pos_files pos_* --y_outFile y_Luo_Homo.npz --pos_outFile pos_Luo_Homo.npz
+python CombineEncodedLabels.py --y_files y_* --pos_files pos_* --y_outFile y_Luo_Homo.npz --pos_outFile pos_Luo_Homo.npz
