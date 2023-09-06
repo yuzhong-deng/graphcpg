99c04aed3c17e792e5ecf233036a51f2f35fef8f
Bruce Deng <57375920+yuzhong-deng@users.noreply.github.com>
2023-09-06 16:38:24 +0800

    Update encodeHomoLuo.sh

--- data/Neuron-Homo/encodeHomoLuo.sh
+++ data/Neuron-Homo/encodeHomoLuo.sh
@@ -7,6 +7,6 @@ do
     if test "$count" = "211"
     then
         echo "Process:" $count $cell
-        python ../EncodeLabelsLuo.py $cell X_HCC.npz y_"$count".npz pos_"$count".npz --prepend_chr --chroms 10 11 12 13 14 15 16 17 18 19 1 20 21 22 2 3 4 5 6 7 8 9 X Y 
+        python EncodeLabelsLuo.py $cell X_HCC.npz y_"$count".npz pos_"$count".npz --prepend_chr --chroms 10 11 12 13 14 15 16 17 18 19 1 20 21 22 2 3 4 5 6 7 8 9 X Y 
     fi
 done
